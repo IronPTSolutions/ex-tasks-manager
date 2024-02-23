8d16bd1e82a53be186dbb2287c9937ab5ca1377f
Julio García Pérez <julio@local.com>
2024-02-23 14:50:01 +0100

    feat: login

--- app.js
+++ app.js
@@ -1,37 +1,44 @@
 // Load env vars
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const logger = require('morgan');
-const mongoose = require('mongoose');
-const createError = require('http-errors');
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const createError = require("http-errors");
+
+const authMiddleware = require("./middlewares/auth.middleware");
 
 // Init configurations
-require('./configs/hbs.config');
-require('./configs/db.config');
+require("./configs/hbs.config");
+require("./configs/db.config");
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set('views', `${__dirname}/views`);
+app.set("view engine", "hbs");
+app.set("views", `${__dirname}/views`);
 
 // Application middlewares
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.urlencoded());
 app.use((req, res, next) => {
   res.locals.currentPath = req.path;
   next();
-})
+});
+
+app.use(authMiddleware.loadUser);
 
 // Application routes
-const routes = require('./configs/routes.config');
-app.use('/', routes);
+const routes = require("./configs/routes.config");
+app.use("/", routes);
 
-app.use((req, res, next) => next(createError(404, 'Route not found')));
+app.use((req, res, next) => next(createError(404, "Route not found")));
 
 app.use((error, req, res, next) => {
-  if (error instanceof mongoose.Error.CastError && error.message.includes("_id")) {
-    error = createError(404, 'Resource not found');
+  if (
+    error instanceof mongoose.Error.CastError &&
+    error.message.includes("_id")
+  ) {
+    error = createError(404, "Resource not found");
   } else if (!error.status) {
     error = createError(500, error);
   }
