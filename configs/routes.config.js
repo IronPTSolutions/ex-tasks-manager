1f3943895b98b221b86960b1ab41cc89c25a58d1
Carlos del Prado <carlos.delpradomota@telefonica.com>
2024-03-02 00:47:11 +0000

    feat: finder, helpers & styles

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -7,28 +7,24 @@ const secure = require('../middlewares/auth.middleware');
 const router = express.Router();
 
 // Issue CRUD
-// Create
 router.get('/create-issue', secure.isAuthenticated, issues.create);
 router.post('/create-issue', secure.isAuthenticated, issues.doCreate);
-// Read
 router.get('/issues', secure.isAuthenticated, issues.list);
 router.get('/issues/:id', secure.isAuthenticated, issues.detail);
-// Update
 router.get('/issues/:id/edit', secure.isAuthenticated, issues.edit);
 router.post('/issues/:id/edit', secure.isAuthenticated, issues.doEdit);
-// Delete
-router.get('/issues/:id/delete', secure.isAuthenticated, issues.delete);
+router.post('/issues/:id/delete', secure.isAuthenticated, issues.delete);
 
+// Issue messages CRUD
 router.post('/issues/:issueId/messages', secure.isAuthenticated, messages.doCreate);
+router.post('/issues/:issueId/messages/:id/delete', secure.isAuthenticated, messages.doDelete);
 
 // User CRUD
 router.get('/login', users.login);
 router.post('/login', users.doLogin);
-router.get('/logout', users.logout);
-// Create
 router.get('/signup', users.create);
 router.post('/signup', users.doCreate);
-
+router.get('/logout', users.logout);
 router.get('/profile', secure.isAuthenticated, users.profile);
 
 router.get('/', (req, res, next) => res.redirect('/issues'));
--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -24,5 +24,29 @@ module.exports.doCreate = (req, res, next) => {
           });
       }
     }).catch(next);
-    
+}
+
+module.exports.doDelete = (req, res, next) => {
+  const { issueId, id } = req.params;
+  Issue.findById(issueId)
+    .then((issue) => {
+      if (!issue) {
+        next(createError(404, 'Issue not found'));
+      } else {
+        const message = req.body;
+        message.owner = req.user.id;
+        message.issue = issueId;
+        return Message.findOne({ _id: id, issue: issueId })
+          .then((message) => {
+            if (!message) {
+              next(createError(404, 'Message not found'));
+            } else if (message.owner != req.user.id) {
+              next(createError(403, 'Forbidden'));
+            } else {
+              return Message.deleteOne({ _id: id })
+                .then(() => res.redirect(`/issues/${issueId}`))
+            }
+          })
+      }
+    }).catch(next);
 }
